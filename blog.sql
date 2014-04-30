77b89608f41435adf3cc6cbad02374650f9f89ba
Dani <dani.nugrahadi@gmail.com>
2014-05-01 01:31:24 +0700

    Update blog.sql

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 26, 2014 at 08:37 PM
+-- Generation Time: Apr 30, 2014 at 08:22 PM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.5
 
@@ -33,14 +33,16 @@ CREATE TABLE IF NOT EXISTS `artikel` (
   `ISI` text NOT NULL,
   `ID_USER` int(11) NOT NULL,
   PRIMARY KEY (`ID_ARTIKEL`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `artikel`
 --
 
 INSERT INTO `artikel` (`ID_ARTIKEL`, `JUDUL`, `ISI`, `ID_USER`) VALUES
-(1, 'test 3', 'test 3', 0);
+(2, 'test', 'tes dan', 5),
+(3, 'test', 'test', 22),
+(4, 'test', 'test', 1);
 
 -- --------------------------------------------------------
 
@@ -61,12 +63,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `NUM_TAMBAH_ART` int(11) NOT NULL,
   `NUM_EDIT_ART` int(11) NOT NULL,
   `LEVEL_USER` int(11) NOT NULL,
+  `kode` varchar(100) NOT NULL,
   PRIMARY KEY (`ID_USER`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`ID_USER`, `NAMA`, `USERNAME`, `EMAIL`, `PASSWORD`, `ALAMAT`, `KOTA`, `AKTIF`, `NUM_LOGIN`, `NUM_TAMBAH_ART`, `NUM_EDIT_ART`, `LEVEL_USER`) VALUES
-(1, 'dani', 'admin', 'admin@gmail.com', 'admin', '', '', 1, 0, 0, 0, 1);
+INSERT INTO `user` (`ID_USER`, `NAMA`, `USERNAME`, `EMAIL`, `PASSWORD`, `ALAMAT`, `KOTA`, `AKTIF`, `NUM_LOGIN`, `NUM_TAMBAH_ART`, `NUM_EDIT_ART`, `LEVEL_USER`, `kode`) VALUES
+(1, 'dani', 'admin', 'admin@gmail.com', 'admin', '', '', 1, 0, 0, 0, 1, ''),
+(25, 'dani', 'danzevo', 'dani.nugrahadi@gmail.com', 'danzevo', 'jl kircon', 'bandung', 1, 0, 0, 0, 2, 'f73610399828660b4cca02a0f711db1d');
